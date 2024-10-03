c3629d8165a7dc9f9c0a6408340cee0f7f5666ea
robertmatov <robertmuwanga7@gmail.com>
2024-10-03 17:31:33 +0300

    Add files via upload

---
+++ Controlstructures.java
@@ -0,0 +1,14 @@
+package controlstructures;
+
+public class Controlstructures {
+
+	public static void main(String[] args) {
+		// Loop to print even numbers 1 to 10
+		for (int x = 2; x <= 10; x += 2) {
+			System.out.println(x);
+		}
+	
+	
+	}
+
+}
